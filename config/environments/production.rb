238552b4de2dc0ff33e5bf16004ec049a10e91c8
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-07-07 11:21:48 +0400

    add mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,8 @@ Rails.application.configure do
 
 config.action_mailer.raise_delivery_errors = true
 # config.action_mailer.default_options = {from: 'no-reply@example.com'}
-
+config.action_mailer.perform_deliveries = true
+ config.active_job.queue_adapter = :sidekiq
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
