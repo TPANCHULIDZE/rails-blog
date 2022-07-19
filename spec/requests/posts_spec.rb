cdb362fc702ae61b62066ef8f174c9ed0d59c7fc
Tato Panchulidze <tpanchulidze@unisens.ge>
2022-07-19 09:36:59 +0400

    start

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,15 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :request do
-    let(:author) { FactoryBot.create(:user) }
-    let(:valid_attributes) {
-        { :title => "first", :body => "new post",  user_id: author.id }
-    }
-
-    let(:valid_session) { {} }
-   describe "GET #index" do
-      it "returns a success response" do
-        expect(author.username).to eq("member")
-      end
-    end
+   
 end
